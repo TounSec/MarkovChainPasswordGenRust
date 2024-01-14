e9f6c65369bf860ffbbde42bd4facea626af34e1
TounSec <hackethique@gmail.com>
2024-01-14 22:57:45 +0100

    MarkovChainPasswordGenRust : v1.0

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,58 @@
 use clap::Parser;
-use rand::prelude::*;
-use rand::distributions::{Distribution, Uniform};
+use markov_generator::{AddEdges, Chain};
+use std::fs::File;
+use std::path::Path;
+use std::io::{BufRead, BufReader};
+use std::process::exit;
 use color_print::cprintln;
 
 // Struct
 #[derive(Parser, Debug)]
-#[clap(version = "1.0", author = "TounSec", about = "🔢Generates a random password with Markov Chain method🔢")]
+#[command(version = "1.0", author = "TounSec", about = "🔢Generates a random password with wordlist and Markov Chain method🔢")]
 struct Args {
     /// Sets length of the password
-    #[clap(short, long, default_value = "16")]
-    length: usize
+    #[arg(short, long, default_value = "16")]
+    length: usize,
+    /// Specifies the path of the wordlist
+    #[arg(short, long)]
+    wordlist: String
 }
 
 // Const
-const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+const DEPTH: usize = 6;
 
-// Generate password function
-fn generate_password(length: usize) -> String
+fn generate_password(length: usize, wordlist: String) -> String
 {
-    let mut rng = thread_rng();
-    let index = Uniform::from(0..CHARSET.len());
+    let mut chain = Chain::new(DEPTH);
+    let path = Path::new(&wordlist);
 
-    (0..length)
-        .map(|_| {
-            let idx = index.sample(&mut rng);
-            CHARSET[idx] as char
-        })
-        .collect()
+    if !path.exists() {
+        eprintln!("❗Error : The path {:?} does not exist", &path);
+        exit(1);
+    }
+
+    let file = File::open(&path).expect("❗Error : The file doesn't exist");
+    let buffer = BufReader::new(file);
+
+    for line in buffer.lines() {
+        let line = line.unwrap();
+        chain.add_all(line.chars(), AddEdges::Both);
+    }
+
+    let mut password = String::new();
+
+    while password.len() < length {
+        let generated = chain.generate()
+            .collect::<String>();
+        password += &generated;
+    }
+
+    password.truncate(length);
+    password
 }
 
-fn main()
-{
+fn main() {
     let args = Args::parse();
-    let password = generate_password(args.length);
+    let password = generate_password(args.length, args.wordlist);
     cprintln!("Generated password : <green>{}</green>", password);
 }
